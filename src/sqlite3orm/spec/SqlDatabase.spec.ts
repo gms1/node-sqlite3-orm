0ace907c4639926ec240c0fdf63e479d3cfc250a
gms1 <www.gms@gmx.at>
2016-12-17 21:36:03 +0100

    test: added tests for SqlDatabase.transactional

--- src/sqlite3orm/spec/SqlDatabase.spec.ts
+++ src/sqlite3orm/spec/SqlDatabase.spec.ts
@@ -2,8 +2,8 @@
 // disabled for tslint 402, because of
 //   https://github.com/palantir/tslint/issues/1572
 
-import {SQL_MEMORY_DB_PRIVATE, SqlDatabase} from '../SqlDatabase';
-import {SqlStatement} from '../SqlStatement';
+import { SQL_MEMORY_DB_PRIVATE, SqlDatabase } from '../SqlDatabase';
+import { SqlStatement } from '../SqlStatement';
 
 // ---------------------------------------------
 
@@ -11,15 +11,15 @@ describe('test SqlDatabase', () => {
   let sqldb: SqlDatabase;
 
   // ---------------------------------------------
-  beforeAll(async(done) => {
+  beforeAll(async (done) => {
     try {
       sqldb = new SqlDatabase();
       await sqldb.open(SQL_MEMORY_DB_PRIVATE);
       await sqldb.exec(
-          'CREATE TABLE TEST (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, col VARCHAR(50))');
+        'CREATE TABLE TEST (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, col VARCHAR(50))');
       await sqldb.run(
-          'INSERT INTO TEST (id, col) values (:a, :b)',
-          {':a': 0, ':b': 'testvalue 0'});
+        'INSERT INTO TEST (id, col) values (:a, :b)',
+        { ':a': 0, ':b': 'testvalue 0' });
     } catch (err) {
       fail(err);
     }
@@ -27,13 +27,13 @@ describe('test SqlDatabase', () => {
   });
 
   // ---------------------------------------------
-  it('expect insert without parameter to succeed', async(done) => {
+  it('expect insert without parameter to succeed', async (done) => {
     try {
       let res = await sqldb.run(
-          'INSERT INTO TEST (id,col) values (1,\'testvalue 1/1\')');
+        'INSERT INTO TEST (id,col) values (1,\'testvalue 1/1\')');
       expect(res.lastID).toBe(1);
       let row =
-          await sqldb.get('SELECT col from TEST where id=:id', {':id': 1});
+        await sqldb.get('SELECT col from TEST where id=:id', { ':id': 1 });
       expect(row.col).toBe('testvalue 1/1');
     } catch (err) {
       fail(err);
@@ -43,22 +43,22 @@ describe('test SqlDatabase', () => {
 
   // ---------------------------------------------
   it('expect insert without parameter to violate unique constraint',
-     async(done) => {
-       try {
-         let res = await sqldb.run(
-             'INSERT INTO TEST (id,col) values (1,\'testvalue 1/1\')');
-         fail();
-       } catch (err) {
-         expect('' + err).toContain('UNIQUE constraint');
-       }
-       done();
-     });
+    async (done) => {
+      try {
+        let res = await sqldb.run(
+          'INSERT INTO TEST (id,col) values (1,\'testvalue 1/1\')');
+        fail();
+      } catch (err) {
+        expect('' + err).toContain('UNIQUE constraint');
+      }
+      done();
+    });
 
   // ---------------------------------------------
-  it('expect insert without primary key to succeed', async(done) => {
+  it('expect insert without primary key to succeed', async (done) => {
     try {
       let res =
-          await sqldb.run('INSERT INTO TEST (col) values (\'testvalue 2\')');
+        await sqldb.run('INSERT INTO TEST (col) values (\'testvalue 2\')');
       expect(res.lastID).toBe(2);
     } catch (err) {
       fail(err);
@@ -68,10 +68,10 @@ describe('test SqlDatabase', () => {
 
 
   // ---------------------------------------------
-  it('expect update with parameter to succeed', async(done) => {
+  it('expect update with parameter to succeed', async (done) => {
     try {
       let res = await sqldb.run(
-          'UPDATE TEST set col = ? WHERE id=?', ['testvalue 1/2', 1]);
+        'UPDATE TEST set col = ? WHERE id=?', ['testvalue 1/2', 1]);
       expect(res.changes).toBe(1);
       let row = await sqldb.get('SELECT col from TEST where id=?', 1);
       expect(row.col).toBe('testvalue 1/2');
@@ -82,10 +82,10 @@ describe('test SqlDatabase', () => {
   });
 
   // ---------------------------------------------
-  it('expect prepared update with parameter to succeed', async(done) => {
+  it('expect prepared update with parameter to succeed', async (done) => {
     try {
       let stmt = await sqldb.prepare('UPDATE TEST set col = $col WHERE ID=$id');
-      let res = await stmt.run({$id: 1, $col: 'testvalue 1/3'});
+      let res = await stmt.run({ $id: 1, $col: 'testvalue 1/3' });
       expect(res.changes).toBe(1);
       await stmt.finalize();
       let row = await sqldb.get('SELECT col from TEST where id=?', 1);
@@ -96,7 +96,7 @@ describe('test SqlDatabase', () => {
     done();
   });
   // ---------------------------------------------
-  it('expect select without parameter to succeed', async(done) => {
+  it('expect select without parameter to succeed', async (done) => {
     try {
       let res = await sqldb.all('SELECT id, col from TEST order by id');
       expect(res.length).toBeGreaterThan(1);
@@ -112,20 +112,20 @@ describe('test SqlDatabase', () => {
 
 
   // ---------------------------------------------
-  it('expect select with parameter to succeed', async(done) => {
+  it('expect select with parameter to succeed', async (done) => {
     try {
       let res: any[] = [];
 
       let count = await sqldb.each(
-          'SELECT id, col from TEST where id >= ? order by id', [0],
-          (err, row) => {
-            if (err) {
-              err.message = 'error row: ' + err.message;
-              fail(err);
-            } else {
-              res.push(row);
-            }
-          });
+        'SELECT id, col from TEST where id >= ? order by id', [0],
+        (err, row) => {
+          if (err) {
+            err.message = 'error row: ' + err.message;
+            fail(err);
+          } else {
+            res.push(row);
+          }
+        });
       expect(count).toBeGreaterThan(1);
       expect(res[0].id).toBe(0);
       expect(res[0].col).toBe('testvalue 0');
@@ -138,17 +138,61 @@ describe('test SqlDatabase', () => {
     done();
   });
 
+  // ---------------------------------------------
   it('expect getting and setting PRAGMA user_version to succeed',
-     async(done) => {
-       try {
-         let oldver = await sqldb.getUserVersion();
-         expect(oldver).toBe(0);
-         await sqldb.setUserVersion(oldver + 3);
-         let newver = await sqldb.getUserVersion();
-         expect(newver).toBe(oldver + 3);
-       } catch (err) {
-         fail(err);
-       }
-       done();
-     });
+    async (done) => {
+      try {
+        let oldver = await sqldb.getUserVersion();
+        expect(oldver).toBe(0);
+        await sqldb.setUserVersion(oldver + 3);
+        let newver = await sqldb.getUserVersion();
+        expect(newver).toBe(oldver + 3);
+      } catch (err) {
+        fail(err);
+      }
+      done();
+    });
+
+  // ---------------------------------------------
+  it('expect transaction to commit on end',
+    async (done) => {
+      try {
+        let oldver = await sqldb.getUserVersion();
+
+        await sqldb.transactionalize(async () => {
+          await sqldb.setUserVersion(oldver + 3);
+        });
+
+        let newver = await sqldb.getUserVersion();
+        expect(oldver + 3).toBe(newver);
+      } catch (err) {
+        fail(err);
+      }
+      done();
+    });
+
+  // ---------------------------------------------
+  it('expect transaction to rollback on error',
+    async (done) => {
+      try {
+        let oldver = await sqldb.getUserVersion();
+
+        try {
+          await sqldb.transactionalize(async () => {
+            await sqldb.setUserVersion(oldver + 3);
+            throw new Error('do not commit');
+          });
+          fail('unexpected');
+        } catch (err2) {
+          let newver = await sqldb.getUserVersion();
+          expect(oldver).toBe(newver);
+        }
+      } catch (err) {
+        fail(err);
+      }
+      done();
+    });
+
+  // ---------------------------------------------
+
 });
